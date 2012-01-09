ac5a3a4f58beeec37dd93e7f5c847eefbe32e941
Conrad Dean <conrad.p.dean@gmail.com>
2012-01-08 20:04:50 -0600

    fancier debug state

--- colorfluency.js
+++ colorfluency.js
@@ -4,31 +4,34 @@
 	      mode();
 	    },
 
-    GAME_MODE_INPUT : function(){
-			// setup event handlers
-			$(document).ready(function(){
-			  // start typing from anywhere
-			  $(document).keydown(function(e){
-			    $("#player input").focus();
-			  });
+    GAME_MODE_INPUT_DEBUG : function(){
+			      // setup event handlers
+			      $(document).ready(function(){
+				// start typing from anywhere
+				$(document).keydown(function(e){
+				  $("#player input").focus();
+				});
 
-			  // bind for player input
-			  $("#player input").keypress(function(e){
-			    // hitting enter submits the attempt
-			    var message = $(this).val();
-			    if(e.keyCode == 13
-			      && message != ""){
-			      cf.log(message);
-			      $(this).val("");
-			    }
-			  });
-			});
+				// bind for player input
+				$("#player input").keypress(function(e){
+				  // hitting enter submits the attempt
+				  var message = $(this).val();
+				  if(e.keyCode == 13
+				    && message != ""){
+				    cf.log(message);
+				    $(this).val("");
+				  }
+				});
+			      });
 
-		      },
+			    },
     log : function(message){
-	var log_item = "<li>"+message+"</li>";
-	$("#logbox").append(log_item);
-      }
+	    $("#logbox").stop();
+	    var log_item = "<li>"+message+"</li>";
+	    $("#logbox").append(log_item);
+	    var lb_height = $("#logbox").prop("scrollHeight");
+	    $("#logbox").animate({scrollTop : lb_height}, 1000);
+	  }
 
   }
 })(window)
