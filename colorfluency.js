6f969e9a080ebd21284691ca324993957544b00d
Conrad Dean <conrad.p.dean@gmail.com>
2012-01-10 01:12:14 -0600

    dumb shorthand parser in place

--- colorfluency.js
+++ colorfluency.js
@@ -25,8 +25,33 @@
 			      });
 
 			    },
+    parseToHexColor : function(input){
+			var in_length = input.length;
+			var out = "";
+			// dumb implementation, before I actually parse the hex
+			switch (in_length){
+			  case 1:
+			    out = input+input+input+input+input+input; //lol
+			    break;
+			  case 3:
+			    var R = input[0];
+			    var G = input[1];
+			    var B = input[2];
+			    out = R+R+ G+G+ B+B; // semi lol
+			    break;
+			  case 6:
+			    out = input;
+			    break;
+			  default:
+			    out = "sorry, input = " + in_length;
+			}
+			return out;
+
+		      },
 
     log : function(message){
+	    var hex_color = cf.parseToHexColor(message);
+	    message = hex_color;
 	    $("#logbox").stop();
 	    var log_item = "<li>"+message+"</li>";
 	    $("#logbox").append(log_item);
